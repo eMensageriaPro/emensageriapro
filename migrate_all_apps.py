b5dbc1b15d3c0e762cdfe0aa605f09a4d20f89f0
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2018-10-27 08:43:38 -0300

    Update migrate_all_apps.py

--- migrate_all_apps.py
+++ migrate_all_apps.py
@@ -93,82 +93,26 @@ def cadastro_controle_acesso():
     print "Controle de acesso atualizado com sucesso!"
 
 
+def reset_sequences():
 
-lista = [
-    'mensageiro',
-    'efdreinf',
-    'esocial',
-    'tabelas',
-    'controle_de_acesso',
-    's1000',
-    's2200',
-    's1030',
-    's1202',
-    's2240',
-    'r1000',
-    'r1070',
-    'r2010',
-    'r2020',
-    'r2030',
-    'r2040',
-    'r2050',
-    'r2060',
-    'r2070',
-    'r2098',
-    'r2099',
-    'r3010',
-    'r5001',
-    'r5011',
-    'r9000',
-    's1005',
-    's1010',
-    's1020',
-    's1035',
-    's1040',
-    's1050',
-    's1060',
-    's1065',
-    's1070',
-    's1080',
-    's1200',
-    's1207',
-    's1210',
-    's1250',
-    's1260',
-    's1270',
-    's1280',
-    's1295',
-    's1298',
-    's1299',
-    's1300',
-    's2190',
-    's2205',
-    's2206',
-    's2210',
-    's2220',
-    's2221',
-    's2230',
-    's2231',
-    's2241',
-    's2245',
-    's2250',
-    's2260',
-    's2298',
-    's2299',
-    's2300',
-    's2306',
-    's2399',
-    's2400',
-    's2405',
-    's2410',
-    's2416',
-    's2420',
-    's3000',
-    's5001',
-    's5002',
-    's5011',
-    's5012',
-]
+    results = executar_sql("""
+        select table_name 
+        from information_schema.tables 
+        where table_schema='public' 
+        and table_name not like 'django_%'
+        and table_name != 'usuarios';
+        """, True)
+
+    for row in results:
+
+        nome_tabela = row[0]
+        results_1 = executar_sql("""SELECT max(id)+1 FROM %s;""" % (nome_tabela), True)
+        quant = results_1[0][0] or 1
+        try:
+            executar_sql("""ALTER SEQUENCE %s_id_seq RESTART WITH %s;""" % (nome_tabela, quant), False)
+        except:
+            pass
+
 
 def migrates():
 
@@ -198,6 +142,7 @@ def migrates():
 
 
 if __name__ == "__main__":
-    migrates()
+    #migrates()
+    reset_sequences()
     cadastro_controle_acesso()
 
