f2530340dced72634a69ca5d311181199980dc1b
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2018-11-30 23:39:24 -0300

    Static files

--- emensageriapro/urls.py
+++ emensageriapro/urls.py
@@ -91,8 +91,6 @@ urlpatterns = [
     url(r'^tabelas/', include('emensageriapro.tabelas.urls')),
     url(r'^esocial/', include('emensageriapro.esocial.urls')),
 
-#URLS
-
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
