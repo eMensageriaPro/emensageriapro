ac6facf5b679047721af810bd9117baa8b4dc924
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2018-11-30 22:59:13 -0300

    Static files

--- emensageriapro/urls.py
+++ emensageriapro/urls.py
@@ -94,5 +94,6 @@ urlpatterns = [
 #URLS
 
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
